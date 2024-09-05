72be9ddc9aabdfcdd16a185daa08215ff169a152
silvi <silviaercolino@yahoo.it>
2024-09-05 21:56:34 +0200

    secondo esercizio di php

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+
+// Selfwork PHP 2
+
+$text = "Marco";
+$text2 = "hai";
+$text3 = "sete";
+$text4 = "?";
+$text5 = "Perchè";
+$text6 = $text2;
+$text7 = 'bevuto';
+$text8 = "tutto";
+
+echo $text." ".$text2." ".$text3.$text4." ".$text5." ".$text6." ".$text7." ".$text8.".";
